e039befc0f38fc82122b86d0e827f2b741aee0f3
Yaroslav Shilov <workyarik@gmail.com>
2020-06-12 20:57:46 +0300

    Updates

---
+++ precache-manifest.e5b17de2d477886e69fd7ab436d9c284.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06cffd54d17effd8844e290ff66625b5",
+    "url": "/Count-New-Words/index.html"
+  },
+  {
+    "revision": "be377b9d4de5629ce0a4",
+    "url": "/Count-New-Words/static/js/2.f439c676.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Count-New-Words/static/js/2.f439c676.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "179bb3754adce5561514",
+    "url": "/Count-New-Words/static/js/main.e7d103c8.chunk.js"
+  },
+  {
+    "revision": "496273c2f72573360dcc",
+    "url": "/Count-New-Words/static/js/runtime-main.61abe20c.js"
+  }
+]);
