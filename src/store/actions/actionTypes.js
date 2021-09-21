6b48d40c40476adff12e50fa254fd933973b68f7
Yaroslav Shilov <workyarik@gmail.com>
2021-09-21 16:22:42 +0300

    I don't know how to add json-server

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -1,7 +1,7 @@
-export const PLUS = 'PLUS';
-export const MINUS = 'MINUS';
-export const DELETE = 'DELETE';
-export const ADD = 'ADD';
-export const EDIT = 'EDIT';
-export const UPLOAD = 'UPLOAD';
-export const DOWNLOAD = 'DOWNLOAD';
+export const PLUS = "PLUS";
+export const MINUS = "MINUS";
+export const DELETE = "DELETE";
+export const ADD = "ADD";
+export const EDIT = "EDIT";
+export const UPLOAD = "UPLOAD";
+export const DOWNLOAD = "DOWNLOAD";
--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -1,9 +1,22 @@
-import {ADD, DELETE, DOWNLOAD, EDIT, MINUS, PLUS, UPLOAD} from "./actionTypes";
+import {
+  ADD,
+  DELETE,
+  DOWNLOAD,
+  EDIT,
+  MINUS,
+  PLUS,
+  UPLOAD,
+} from "./actionTypes";
 
-export const countPlus = (id) => ({type: PLUS, id}); 
-export const countMinus = (id) => ({type: MINUS, id}); 
-export const countDelete = (id) => ({type: DELETE, id}); 
-export const countAdd = (word, meaning) => ({type: ADD, word, meaning});
-export const itemEdit = (id, word, meaning) => ({type: EDIT, id, word, meaning});
-export const localUpload = () => ({type: UPLOAD}); 
-export const localDownload = () => ({type: DOWNLOAD}); 
+export const countPlus = (id) => ({ type: PLUS, id });
+export const countMinus = (id) => ({ type: MINUS, id });
+export const countDelete = (id) => ({ type: DELETE, id });
+export const countAdd = (word, meaning) => ({ type: ADD, word, meaning });
+export const itemEdit = (id, word, meaning) => ({
+  type: EDIT,
+  id,
+  word,
+  meaning,
+});
+export const localUpload = () => ({ type: UPLOAD });
+export const localDownload = (data) => ({ type: DOWNLOAD, data });
