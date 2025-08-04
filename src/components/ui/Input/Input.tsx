e0f8366e1b03cad1c0b7d7d4f87c655494922889
Yaroslav Shilov <workyarik@gmail.com>
2025-08-04 22:31:53 +0400

    Updates

--- src/components/ui/Input/Input.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./Input.module.css";
-
-export const Input = ({
-  ...props
-}: React.InputHTMLAttributes<HTMLInputElement>) => (
-  <input className={styles.input} {...props} />
-);
--- src/store/context.ts
+++
@@ -1,6 +0,0 @@
-import React, { createContext } from "react";
-import type { Action } from "./reducer.ts";
-
-export const DispatchContext = createContext<React.Dispatch<Action>>(() => {
-  throw new Error("DispatchContext value={dispatch} is missing");
-});
